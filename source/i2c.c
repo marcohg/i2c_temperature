dbc22d38496105b880969bd1e071b78fe8d7cb77
marcohg <marco.henrygin@gmail.com>
2025-06-15 23:08:04 -0700

    refactor i2c to use most of config tools

--- source/i2c.c
+++ source/i2c.c
@@ -2,10 +2,16 @@
 #include "fsl_debug_console.h"
 #include "peripherals.h"
 
+static lpi2c_master_transfer_t master_transfer = {
+    .slaveAddress = I2C_MASTER_SLAVE_ADDR_7BIT,
+    .direction = kLPI2C_Write,
+    .subaddress = 0x01,
+    .subaddressSize = 1,
+    .data = lpi2c1_master_buffer,
+    .dataSize = I2C_DATA_LENGTH,
+    .flags = kLPI2C_TransferDefaultFlag,
+};
 
-
-//uint8_t g_master_rxBuff[I2C_DATA_LENGTH];
-//lpi2c_master_handle_t g_m_handle;
 volatile bool g_MasterCompletionFlag = false;
 volatile bool g_MasterNackFlag = false;
 
@@ -23,12 +29,7 @@ void lpi2c1_master_callback(LPI2C_Type *base, lpi2c_master_handle_t *handle,
 }
 
 int i2c_test() {
-  lpi2c_master_transfer_t masterXfer = { 0 }; // use peripheral settings
   status_t reVal = kStatus_Fail;
-
-  /*Clock setting for LPI2C*/
-//  CLOCK_SetMux(kCLOCK_Lpi2cMux, LPI2C_CLOCK_SOURCE_SELECT);   // not set by peripheral yet
-//  CLOCK_SetDiv(kCLOCK_Lpi2cDiv, LPI2C_CLOCK_SOURCE_DIVIDER);
 
   PRINTF("\r\nLPI2C board2board interrupt example -- Master transfer.\r\n");
 
@@ -49,43 +50,10 @@ int i2c_test() {
   }
   PRINTF("\r\n\r\n");
 
-//  lpi2c_master_config_t masterConfig;
-
-  /*
-   * masterConfig.debugEnable = false;
-   * masterConfig.ignoreAck = false;
-   * masterConfig.pinConfig = kLPI2C_2PinOpenDrain;
-   * masterConfig.baudRate_Hz = 100000U;
-   * masterConfig.busIdleTimeout_ns = 0;
-   * masterConfig.pinLowTimeout_ns = 0;
-   * masterConfig.sdaGlitchFilterWidth_ns = 0;
-   * masterConfig.sclGlitchFilterWidth_ns = 0;
-   */
-//  LPI2C_MasterGetDefaultConfig(&masterConfig);
-
-  /* Change the default baudrate configuration */
-//  masterConfig.baudRate_Hz = I2C_BAUDRATE;
-
-  /* Initialize the LPI2C master peripheral */
-//  LPI2C_MasterInit(EXAMPLE_I2C_MASTER, &masterConfig,  LPI2C_MASTER_CLOCK_FREQUENCY);
-
-  /* Create the LPI2C handle for the non-blocking transfer */
-//  LPI2C_MasterTransferCreateHandle(EXAMPLE_I2C_MASTER, &g_m_handle,lpi2c_master_callback, NULL);
-
-  /* subAddress = 0x01, data = g_master_txBuff - write to slave.
-   start + slaveaddress(w) + subAddress + length of data buffer + data buffer + stop*/
-  uint8_t deviceAddress = 0x01U;
-  masterXfer.slaveAddress = I2C_MASTER_SLAVE_ADDR_7BIT;
-  masterXfer.direction = kLPI2C_Write;
-  masterXfer.subaddress = (uint32_t) deviceAddress;
-  masterXfer.subaddressSize = 1;
-  masterXfer.data = lpi2c1_master_buffer;
-  masterXfer.dataSize = I2C_DATA_LENGTH;
-  masterXfer.flags = kLPI2C_TransferDefaultFlag;
-
   /* Send master non-blocking data to slave */
-//  reVal = LPI2C_MasterTransferNonBlocking(EXAMPLE_I2C_MASTER, &g_m_handle, &masterXfer);
-  reVal = LPI2C_MasterTransferNonBlocking(LPI2C1, &lpi2c1_master_handle, &masterXfer);
+  master_transfer.direction = kLPI2C_Write;
+  master_transfer.dataSize = I2C_DATA_LENGTH;
+  reVal = LPI2C_MasterTransferNonBlocking(LPI2C1, &lpi2c1_master_handle, &master_transfer);
 
   if (reVal != kStatus_Success) {
     return -1;
@@ -103,16 +71,9 @@ int i2c_test() {
 
   /* subAddress = 0x01, data = g_master_rxBuff - read from slave.
    start + slaveaddress(w) + subAddress + repeated start + slaveaddress(r) + rx data buffer + stop */
-  masterXfer.slaveAddress = I2C_MASTER_SLAVE_ADDR_7BIT;
-  masterXfer.direction = kLPI2C_Read;
-  masterXfer.subaddress = (uint32_t) deviceAddress;
-  masterXfer.subaddressSize = 1;
-  masterXfer.data = lpi2c1_master_buffer;
-  masterXfer.dataSize = I2C_DATA_LENGTH - 1;
-  masterXfer.flags = kLPI2C_TransferDefaultFlag;
-
-//  reVal = LPI2C_MasterTransferNonBlocking(EXAMPLE_I2C_MASTER, &lpi2c1_master_handle,&masterXfer);
-  reVal = LPI2C_MasterTransferNonBlocking(EXAMPLE_I2C_MASTER, &lpi2c1_master_handle,&masterXfer);
+  master_transfer.direction = kLPI2C_Read;
+  master_transfer.dataSize = I2C_DATA_LENGTH - 1;
+  reVal = LPI2C_MasterTransferNonBlocking(LPI2C1, &lpi2c1_master_handle,&master_transfer);
   if (reVal != kStatus_Success) {
     return -1;
   }
